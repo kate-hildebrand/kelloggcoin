09257c73b2bb83fb360757f0d34a60eee053bb95
kate-hildebrand <kate.hildebrand@kellogg.northwestern.edu>
2022-04-23 23:48:17 +0000

    final result

--- practice.rb
+++ practice.rb
@@ -7,14 +7,14 @@
 # with no from_user are from the "ICO" (Initial Coin Offering)
 
 blockchain = [
-  { from_user: nil, to_user: "ben", amount: 20000 },
-  { from_user: nil, to_user: "brian", amount: 20000 },
-  { from_user: "ben", to_user: "evan", amount: 9000 },
-  { from_user: "brian", to_user: "anthony", amount: 7000 },
-  { from_user: "evan", to_user: "anthony", amount: 400 },
-  { from_user: "ben", to_user: "anthony", amount: 1500 },
-  { from_user: "anthony", to_user: "ben", amount: 4500 },
-  { from_user: "anthony", to_user: "evan", amount: 1750 }
+  { from_user: nil, to_user: "ben", amount: 20000 }, #0
+  { from_user: nil, to_user: "brian", amount: 20000 }, #1
+  { from_user: "ben", to_user: "evan", amount: 9000 }, #2
+  { from_user: "brian", to_user: "anthony", amount: 7000 }, #3
+  { from_user: "evan", to_user: "anthony", amount: 400 }, #4
+  { from_user: "ben", to_user: "anthony", amount: 1500 },#5
+  { from_user: "anthony", to_user: "ben", amount: 4500 },#6
+  { from_user: "anthony", to_user: "evan", amount: 1750 } #7
 ]
 
 # Write code below that returns the number of KelloggCoin that each user has in their 
@@ -27,3 +27,40 @@ blockchain = [
 # Anthony's KelloggCoin balance is 2650
 
 # 👇👇👇 Your code HERE 👇👇👇
+
+amount1 = blockchain [0][:amount] 
+amount1 = amount1.to_f
+amount2 = blockchain [1][:amount] 
+amount2 = amount2.to_f
+amount3 = blockchain [2][:amount] 
+amount3 = amount3.to_f
+amount4 = blockchain [3][:amount] 
+amount4 = amount4.to_f
+amount5 = blockchain [4][:amount] 
+amount5 = amount5.to_f
+amount6 = blockchain [5][:amount] 
+amount6 = amount6.to_f
+amount7 = blockchain [6][:amount] 
+amount7 = amount7.to_f
+amount8 = blockchain [7][:amount] 
+amount8 = amount8.to_f
+
+#add together Ben's transactions
+ben_amount = amount1 - amount3 - amount6 + amount7
+ben_final = "Ben's KelloggCoin balance is $#{ben_amount}"
+puts ben_final
+
+#add together Brian's's transactions
+brian_amount = amount2 - amount4
+brian_final = "Brian's KelloggCoin balance is $#{brian_amount}"
+puts brian_final
+
+#add together Evan's transactions
+evan_amount = amount3 - amount5 + amount8
+evan_final = "Evan's KelloggCoin balance is $#{evan_amount}"
+puts evan_final
+
+#add together Anthony's's transactions
+anythony_amount = amount4 + amount5 + amount6 - amount7 - amount8
+anthony_final = "Anthony's KelloggCoin balance is $#{anythony_amount}"
+puts anthony_final
